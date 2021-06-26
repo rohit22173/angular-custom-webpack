e57424e8b25dcfb6ac3360a3c5cb4471fb037c80
Rohit <rohit22173@gmail.com>
2021-06-27 00:27:13 +0530

    Added loaders and updated the dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,41 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build')
   },
-  optimization: {
-    minimize: true,
-    runtimeChunk: true
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.css$/,
+        use: [
+          // [style-loader](/loaders/style-loader)
+          { loader: 'style-loader' },
+          // [css-loader](/loaders/css-loader)
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          // [sass-loader](/loaders/sass-loader)
+          { loader: 'sass-loader' }
+        ]
+      }
+    ] 
+  }
+  
 };
